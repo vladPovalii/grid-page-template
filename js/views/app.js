c71bdf91c83ed1385c991f0afaf181f4f9d66cfd
VladPovalii <vladpovalii@gmail.com>
2016-11-05 23:56:52 +0200

    remove views to separate files

--- js/views/app.js
+++ js/views/app.js
@@ -60,28 +60,7 @@ define(["jquery", "underscore", "backbone", "classie", "Modernizr"], function($,
 			}
 		});
 
-		var GridItemView = Backbone.View.extend({
-			tagName: "div",
 
-			className: "grid__item",
-
-			tpl: _.template($("#grid-item-template").html()),
-
-			events: "",
-
-			initialize: function(){
-
-			},
-
-			render: function(){
-				this.$el.html(this.tpl({
-					title: "test title",
-					category: "test category",
-					createdOnFormated: "4 november"
-				}));
-				return this.el;
-			}
-		});
 		*/
 
 		// gets the viewport width and height
