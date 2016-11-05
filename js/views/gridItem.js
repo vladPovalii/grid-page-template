120c52af886f6190643858934282e5a05e32c332
VladPovalii <vladpovalii@gmail.com>
2016-11-05 23:57:11 +0200

    add gridItem view

---
+++ js/views/gridItem.js
@@ -0,0 +1,22 @@
+var GridItemView = Backbone.View.extend({
+	tagName: "div",
+
+	className: "grid__item",
+
+	tpl: _.template($("#grid-item-template").html()),
+
+	events: "",
+
+	initialize: function(){
+
+	},
+
+	render: function(){
+		this.$el.html(this.tpl({
+			title: "test title",
+			category: "test category",
+			createdOnFormated: "4 november"
+		}));
+		return this.el;
+	}
+});
