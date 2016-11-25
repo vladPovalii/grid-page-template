e218b6eb5808d9df1f28de91834183fec9e5d65f
Vlad Povalii <vladpovalii@gmail.com>
2016-11-25 21:09:25 +0200

    test csrf protection, and add express-session

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
 var express = require('express'),
+    cookieParser = require('cookie-parser');
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
-    path = require('path');
+    path = require('path'),
+    csrf = require('csurf'),
+    fs = require('fs'),
+    session = require('express-session');
 
-// get cfg with connection information
+// environment
 var env = process.env.NODE_ENV || 'development';
-var config = require('./db_config')[env];
 
-// connect to mongoDB
+// db connection
+var config = require('./config')[env];
 mongoose.connect('mongodb://' + config.database.host + '/' + config.database.db);
-
-//test connection
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("connected!");
+  console.log('connected!');
 });
-
 var Schema = mongoose.Schema;
-
 var NewsSchema = new Schema({
   _id: {type: String},
   index: {type: Number},
@@ -27,13 +27,17 @@ var NewsSchema = new Schema({
   date: {type: Date, default: new Date()},
   createdOnFormatted: {type: String}
 });
-
 mongoose.model('news', NewsSchema);
 var news = mongoose.model('news');
+//
 
-var app = express();
+var app = express(),
+    csrfProtection = csrf({cookie: true});
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(session({secret: config.secretKey, resave: false, saveUninitialized: false}));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
@@ -44,14 +48,15 @@ app.get('/news', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/html', 'news.html'));
 });
 
-app.get('/login', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/html', 'login.html'));
+app.get('/login', csrfProtection, function(req, res) {
+  var html = fs.readFileSync(path.join(__dirname, '/public/html', 'login.html'), 'utf8');
+  res.send(html.replace('{{csrfToken}}', req.csrfToken()));
 });
 
 app.get('/api/news', function(req, res) {
   news.find(function(err, docs) {
     docs.forEach(function(item) {
-      console.log("Received a GET request for _id: " + item._id);
+      console.log('Received a GET request for _id: ' + item._id);
     })
     res.json(docs);
   });
