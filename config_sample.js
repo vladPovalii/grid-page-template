43bb75670dcad4592d767eb80dacfff42b1dd188
Vlad Povalii <vladpovalii@gmail.com>
2016-11-25 21:09:52 +0200

    add session setting

---
+++ config_sample.js
@@ -0,0 +1,35 @@
+var config = {
+    development: {
+        //url to be used in link generation
+        url: 'http://my.site.com',
+        //mongodb connection settings
+        database: {
+            host:   'localhost',
+            port:   '27017',
+            db:     'test'
+        },
+        //server details
+        server: {
+            host: '127.0.0.1',
+            port: '3000'
+        },
+        //secret strings
+        secretKey: 'your secret key'
+    },
+    production: {
+        //url to be used in link generation
+        url: 'http://my.site.com',
+        //mongodb connection settings
+        database: {
+            host: 'localhost',
+            port: '27017',
+            db:   'test'
+        },
+        //server details
+        server: {
+            host:   '127.0.0.1',
+            port:   '3000'
+        }
+    }
+};
+module.exports = config;
